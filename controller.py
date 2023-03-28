a0b3c7c8ea0fddff3c00f2ff094769e1c356d7ce
Eleanor O'Connell <ocoelean@amazon.com>
2023-03-28 11:30:07 +0100

    added function to add record + changes to where functions are in files

--- controller.py
+++ controller.py
@@ -1,5 +1,6 @@
 import service
 import database
+import sys
 
 # defining the while loop that loops through all the menu options 
 def start_app():
@@ -9,22 +10,40 @@ def start_app():
         choice = input("Please select a choice: ")
         if choice == "1":
             print(all_orders())
-        else:
-            exit = True
+        if choice == "2":
+            print(input_record())
+        if choice == "3":
+            confirm_update()
             database.commit_changes()
+            sys.exit("Goodbye for now!")
 
-# defining the all orders function for when the use selects option 1
+# defining the all orders function for when the user selects option 1
 # uses the function definined in the service file
 def all_orders():
     return service.get_all()
+
+# defining the input record function for when the user selects option 2 
+def input_record():
+    shortname = input("Please enter the alias of the new employee: ")
+    first = input("Please enter the first name of the new employee: ")
+    last = input("Please enter the last name of the new employee: ")
+    email = input("Please enter the email address of the new employee: ")
+    level = input("Please enter the level of the new employee: ")
+    years = input("Please enter how many years the employee has been with Amazon: ")
+    ai2 = input("Is the employee in the AI2 program? True or False?: ")
+    return service.create_single_record(shortname, first, last, email, level, years, ai2)
 
 # menu displayed when requests are made to the db
 menu = (
     """
     Welcome to the Phone Tool! Please select a choice.
     1. Read all entries
-    2. Exit
+    2. Enter a new record 
+    3. Exit
     """
 )
+
+def confirm_update():
+    return service.update_db()
 
 start_app()
--- service.py
+++ service.py
@@ -1,6 +1,17 @@
 import database
 
-# defining the get all function by oulling the function from the db file 
+# defining the get all function by pulling the function from the db file 
 def get_all():
-    data = database.check_all_records()
+    query = "SELECT * FROM phonetool"
+    data = database.run_query(query)
     return data
+
+# defining the create single order record by pulling the function from the db file 
+def create_single_record(alias, first_name, last_name, email_address, employee_level, years_of_employment, in_the_AI2_program):
+    query = f"INSERT INTO phonetool (alias, first_name, last_name, email_address, employee_level, years_of_employment, in_the_AI2_program) VALUES ('{alias}', '{first_name}', '{last_name}', '{email_address}', {employee_level}, {years_of_employment}, {in_the_AI2_program})"
+    database.run_query(query)
+    return "The new employee has been recorded!"
+
+def update_db():
+    print("Table has been updated")
+    return database.commit_changes()
