91aab43e20789ee93b0f8d4e23150523e451523f
Eleanor O'Connell <ocoelean@amazon.com>
2023-04-18 11:19:23 +0100

    adding the unit tests and comments on how they work

--- tests_/test_input_record.py
+++ tests_/test_input_record.py
@@ -1,14 +1,14 @@
-# this file will test the functions of controller.py with unit testing
+# this file uses unit testing to test the create_single_record function in the service.py file
 # unit testing only tests single functions and not the entire app
-# to unit test controller.py the function is being mocked 
-# it is best to use a test_db for th
+# to unit test service.py I am using mocking 
+# it is best to use a test_db for this test
 
-# importing the controller and service files for testing
-import service
+# importing the service file for testing
+from service import create_single_record
 
 # creating the testing function to test the record creation function 
 def test_inputRecord(mocker):
-    # arrange - setting the varaibles and their values to but into the test_db
+    # arrange - setting the variables and their values to put into test_db
     test_proven = "Test complete :)"
     test_alias = "namtes"
     test_first_name = "test"
@@ -20,7 +20,7 @@ def test_inputRecord(mocker):
     mocker.patch("service.create_single_record", return_value = test_proven)
 
     # act - setting out what function I want to test
-    result = service.create_single_record(test_alias, test_first_name, test_last_name, test_email_address, test_employee_level, test_years_of_employment, test_in_the_AI2_program)
+    result = create_single_record(test_alias, test_first_name, test_last_name, test_email_address, test_employee_level, test_years_of_employment, test_in_the_AI2_program)
 
     # providing the assumption about what I want the result of the testing to be 
     assert result == test_proven
