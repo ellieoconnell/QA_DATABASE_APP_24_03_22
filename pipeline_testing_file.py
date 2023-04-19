11623dd164185c885945288064cf075b722a741a
Eleanor O'Connell <ocoelean@amazon.com>
2023-04-19 08:30:28 +0100

    adding file to syncing of pipeline

---
+++ pipeline_testing_file.py
@@ -0,0 +1,10 @@
+# creating a file to test that the pipline syncs with source code changes from github
+# adding a simple for loop to iterate over a list 
+
+numbers = [7,15,83,12,22,65,1,4]
+
+count = 0 
+for i in numbers:
+    count += 1
+
+print(count)
